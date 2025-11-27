12e1ff3698924dccfb98cc476252e8f9e757c163
ahmed <ahmedliouane0@email.com>
2025-11-27 19:31:48 +0100

    3malna projet

---
+++ mqtt-chat-demo/src/main/java/com/example/mqtt_chat_demo/MqttChatDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.mqtt_chat_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqttChatDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MqttChatDemoApplication.class, args);
+	}
+
+}
---
+++ mqtt-chat-demo/src/test/java/com/example/mqtt_chat_demo/MqttChatDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mqtt_chat_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqttChatDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
